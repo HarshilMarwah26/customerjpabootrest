4328ae61a3ed7ef7a5d927157bbd954e0a2e8ce9
harshil marwah <marwahharshil@gmail.com>
2020-11-02 09:46:26 +0530

    added @transactional and changes to pom.xml

--- src/main/java/com/cg/customerapp/service/CustomerServiceImpl.java
+++ src/main/java/com/cg/customerapp/service/CustomerServiceImpl.java
@@ -5,41 +5,37 @@ import java.util.List;
 import com.cg.customerapp.util.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.cg.customerapp.entities.Customer;
 import com.cg.customerapp.repository.ICustomerRepository;
 
+@Transactional
 @Service
-public class CustomerServiceImpl implements ICustomerService{
-	
-	
-
+public class CustomerServiceImpl implements ICustomerService {
 
 	@Autowired
 	private ICustomerRepository repository;
-	
-	
+
 	@Override
 	public Customer findById(Long id) {
-		Customer customer=repository.findById(id);
+		Customer customer = repository.findById(id);
 		return customer;
 	}
-
 
 	@Override
 	public List<Customer> findAll() {
-		List<Customer>list=repository.findAll();
+		List<Customer> list = repository.findAll();
 		return list;
 	}
-	
+
 	@Override
 	public Customer register(Customer customer) {
 		ValidationUtil.checkArgumentNotNull(customer);
-        ValidationUtil.checkName(customer.getName());
+		ValidationUtil.checkName(customer.getName());
 		customer = repository.add(customer);
 		return customer;
 	}
-	
 
 	@Override
 	public Customer updateName(Long id, String name) {
